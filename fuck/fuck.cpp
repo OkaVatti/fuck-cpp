8105aef866741c6618cfceccc88a97c1927c0432
parker <greenwoodsky891@gmail.com>
2021-11-02 17:49:50 -0400

    fuck c++

---
+++ fuck/fuck.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int main() {
+    std::cout << "                                  " << std::endl;
+    std::cout << "  FFFFFF  UU  UU   CCCCc   KK KK  " << std::endl;
+    std::cout << "  FF      UU  UU  CCc  CC  KK KK  " << std::endl;
+    std::cout << "  FFFF    UU  UU  CC       KKKK   " << std::endl;
+    std::cout << "  FF      UUUUUU  CCc  CC  KK kK  " << std::endl;
+    std::cout << "  FF       UUUU    CCCCc   KK KK  " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  I should've just gotten a job.  " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  Fuck                            " << std::endl;
+    std::cout << "  /fək/                           " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  Slang                           " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  verb                            " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "   1. to have sex with (someone)  " << std::endl;
+    std::cout << "   2. to ruin or damage           " << std::endl;
+    std::cout << "      (something)                 " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  nouns                           " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  act or instance of having sex   " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  excamation                      " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "  used alone or as a noun or      " << std::endl;
+    std::cout << "  verb in various phrases to      " << std::endl;
+    std::cout << "  express annoyance, contempt,    " << std::endl;
+    std::cout << "  or impatience                   " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "                                  " << std::endl;
+    std::cout << "           'Google's Definition'  " << std::endl;
+    std::cout << "                                  " << std::endl;
+    return 0;
+}
---
+++ hello?/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // put your shit in here i guess, this is fucking gay
+    std::cout << "hey there bitch" << std::endl;
+    return 0;
+}
